2228b309149ad4e24289ae9d17b2efa4f56f39e1
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-04 15:54:44 +0200

    Add bird test

--- core/src/ch/cpnv/angrybirds/AngryWirds.java
+++ core/src/ch/cpnv/angrybirds/AngryWirds.java
@@ -6,32 +6,29 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class AngryWirds extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	float x;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		x = 0;
-	}
+import ch.cpnv.model.Bird;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		x++;
-		batch.draw(img, x, 500);
-		// C'est à ce moment que tous les draw sont affichés
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+public class AngryWirds extends ApplicationAdapter {
+    SpriteBatch batch;
+    Bird bird;
+    
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        bird = new Bird(100, 100, 100, 100);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        bird.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
