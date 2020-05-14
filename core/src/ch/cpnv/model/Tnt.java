77193bd99baf3910ce6f1119ed82b1af2668f31e
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-14 08:22:59 +0200

    Update tnt size

--- core/src/ch/cpnv/model/Tnt.java
+++ core/src/ch/cpnv/model/Tnt.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.math.Vector2;
 public final class Tnt extends PhysicalObject {
     private static final String PICTURE_NAME = "tnt.png";
 
-    private static final int WIDTH = 40;
-    private static final int HEIGHT = 40;
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 60;
 
     private int negativePoints;
 
