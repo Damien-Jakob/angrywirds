ce3a58d94f5c59cd46e27cc0d36f16c6b6a38e48
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-14 09:14:00 +0200

    Draw entire flour with blocks

--- core/src/ch/cpnv/model/Block.java
+++ core/src/ch/cpnv/model/Block.java
@@ -8,6 +8,14 @@ public final class Block extends PhysicalObject {
     private static final int WIDTH = 40;
     private static final int HEIGHT = 40;
 
+    public static int height() {
+        return HEIGHT;
+    }
+
+    public static int width() {
+        return WIDTH;
+    }
+
     public Block(Vector2 position) {
         super(position, WIDTH, HEIGHT, PICTURE_NAME);
     }
