988e0a80a2108c90d5a4b2d96100cbad5addf448
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-14 08:37:21 +0200

    Update MovingObject comments

--- core/src/ch/cpnv/model/MovingObject.java
+++ core/src/ch/cpnv/model/MovingObject.java
@@ -3,8 +3,8 @@ package ch.cpnv.model;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class MovingObject extends PhysicalObject {
-    public final static float GRAVITY = 50f; // Gravity, for objects that fall
-    // 250f
+    // Gravity, for objects that fall
+    public final static float GRAVITY = 50f;
 
     protected Vector2 speed;
     protected boolean frozen; // Allows to temporarily freeze the movement
@@ -31,13 +31,13 @@ public abstract class MovingObject extends PhysicalObject {
         return frozen;
     }
 
-    // met la position égale à la vitesse
     public void move(float dt) {
         if (!isFrozen()) {
             translate(speed.x * dt, speed.y * dt);
         }
     }
 
-    // the accelerate method implements the speed change, which depends on the physics of the derived object, reason why it is abstract here
+    // the accelerate method implements the speed change,
+    // which depends on the physics of the derived object, reason why it is abstract here
     public abstract void accelerate(float dt);
 }
