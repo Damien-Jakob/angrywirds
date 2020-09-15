dd0cd536d9975661234757f6252820e86fa69517
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-15 14:52:45 +0200

    Add voc accessors

--- core/src/ch/cpnv/model/data/Vocabulary.java
+++ core/src/ch/cpnv/model/data/Vocabulary.java
@@ -2,6 +2,8 @@ package ch.cpnv.model.data;
 
 import java.util.ArrayList;
 
+import ch.cpnv.angrybirds.AngryWirds;
+
 public class Vocabulary {
     private String name;
     private ArrayList<Word> words;
@@ -9,5 +11,14 @@ public class Vocabulary {
     public Vocabulary(String name) {
         this.name = name;
         words = new ArrayList<Word>();
+    }
+
+    public void addWord(Word word) {
+        words.add(word);
+    }
+
+    public Word pickAWord() {
+        // TODO better selection
+        return words.get(AngryWirds.alea.nextInt(words.size()));
     }
 }
