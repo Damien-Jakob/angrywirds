fa2f611e123b70580aa76f64bbc8b8eedf406f1f
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-22 14:16:01 +0200

    update Pig constructor

--- core/src/ch/cpnv/model/Pig.java
+++ core/src/ch/cpnv/model/Pig.java
@@ -2,6 +2,10 @@ package ch.cpnv.model;
 
 import com.badlogic.gdx.math.Vector2;
 
+import ch.cpnv.model.data.Word;
+
+// TODO store word ?
+
 public final class Pig extends TextualObject {
     public static final int WIDTH = 60;
     public static final int HEIGHT = WIDTH;
@@ -10,8 +14,8 @@ public final class Pig extends TextualObject {
 
     private int points;
 
-    public Pig(Vector2 position, String word, int points) {
-        super(position, WIDTH, HEIGHT, PICTURE_NAME, word);
+    public Pig(Vector2 position, Word word, int points) {
+        super(position, WIDTH, HEIGHT, PICTURE_NAME, word.getQuestion());
         this.points = points;
     }
 
