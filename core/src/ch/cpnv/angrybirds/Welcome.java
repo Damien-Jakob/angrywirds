faa71f3e3658f0b07f878b1076d154e5df6a041f
Damien Jakob <damien.jakob@cpnv.ch>
2020-10-06 13:55:26 +0200

    Add Welcome page

---
+++ core/src/ch/cpnv/angrybirds/Welcome.java
@@ -0,0 +1,97 @@
+package ch.cpnv.angrybirds;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.Random;
+
+public class Welcome extends Game implements InputProcessor {
+    public static Random alea;
+
+    private Texture background;
+    private BitmapFont font;
+
+    private SpriteBatch batch;
+
+    private OrthographicCamera camera;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, AngryWirds.WORLD_WIDTH, AngryWirds.WORLD_HEIGHT);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        background = new Texture(Gdx.files.internal("background.jpg"));
+    }
+
+    @Override
+    public void render() {
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(background, 0, 0, camera.viewportWidth, camera.viewportHeight);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    // InputProcessor interface implementation
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+    // convert screen coordinates to camera coordinates
+    protected Vector2 convertCoordinates(int screenX, int screenY) {
+        Vector3 point = new Vector3(screenX, screenY, 0);
+        camera.unproject(point);
+        return new Vector2(point.x, point.y);
+    }
+}
