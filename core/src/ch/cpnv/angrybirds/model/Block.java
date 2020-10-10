6ee50899673b23632b6957d9d179f511c2658f87
Damien Jakob <damien.jakob@cpnv.ch>
2020-10-10 21:25:56 +0200

    Add points to collidable physical objects

--- core/src/ch/cpnv/angrybirds/model/Block.java
+++ core/src/ch/cpnv/angrybirds/model/Block.java
@@ -2,13 +2,20 @@ package ch.cpnv.angrybirds.model;
 
 import com.badlogic.gdx.math.Vector2;
 
-public final class Block extends PhysicalObject {
+public final class Block extends PhysicalObject implements ScoreInfluencer{
     private static final String PICTURE_NAME = "block.png";
+
+    public static final int POINTS = -1;
 
     public static final int WIDTH = 30;
     public static final int HEIGHT = WIDTH;
 
     public Block(Vector2 position) {
         super(position, WIDTH, HEIGHT, PICTURE_NAME);
+    }
+
+    @Override
+    public int getPoints() {
+        return POINTS;
     }
 }
