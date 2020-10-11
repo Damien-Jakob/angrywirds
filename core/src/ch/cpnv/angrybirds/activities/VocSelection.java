2fddc1e10967998232f5b397217d7c354d75ce93
Damien Jakob <damien.jakob@cpnv.ch>
2020-10-11 13:46:05 +0200

    Update font colors

--- core/src/ch/cpnv/angrybirds/activities/VocSelection.java
+++ core/src/ch/cpnv/angrybirds/activities/VocSelection.java
@@ -38,6 +38,7 @@ public class VocSelection extends Game implements InputProcessor {
     private float titlePositionX;
 
     private BitmapFont vocabularyFont;
+    private BitmapFont randomVocFont;
     private float vocTextX;
 
     private IconButton randomVocButton;
@@ -60,18 +61,20 @@ public class VocSelection extends Game implements InputProcessor {
         background = new Texture(Gdx.files.internal("background.jpg"));
 
         titleFont = new BitmapFont();
-        titleFont.setColor(Color.RED);
+        titleFont.setColor(Color.GOLD);
         titleFont.getData().setScale(TITLE_SIZE);
         GlyphLayout titleGlyphLayout = new GlyphLayout();
         titleGlyphLayout.setText(titleFont, TITLE);
         titlePositionX = Play.WORLD_WIDTH / 2f - titleGlyphLayout.width / 2f;
 
         vocabularyFont = new BitmapFont();
-        Color vocabularyNameColor = Color.BLUE;
-        vocabularyFont = new BitmapFont();
-        vocabularyFont.setColor(vocabularyNameColor);
+        vocabularyFont.setColor(Color.BLUE);
         vocabularyFont.getData().setScale(LINE_SIZE);
         vocTextX = VOC_POSITION_X + BUTTON_DIMENSION + VOC_MARGIN;
+
+        randomVocFont = new BitmapFont();
+        randomVocFont.setColor(Color.RED);
+        randomVocFont.getData().setScale(LINE_SIZE);
 
         float buttonY = VOC_START_Y;
         randomVocButton = new IconButton(
@@ -112,7 +115,7 @@ public class VocSelection extends Game implements InputProcessor {
         batch.draw(background, 0, 0, camera.viewportWidth, camera.viewportHeight);
         titleFont.draw(batch, TITLE, titlePositionX, TITLE_POSITION_Y);
         randomVocButton.draw(batch);
-        vocabularyFont.draw(batch, "Random", vocTextX, randomVocButton.getYTop() + TEXT_OFFSET_Y);
+        randomVocFont.draw(batch, "Random", vocTextX, randomVocButton.getYTop() + TEXT_OFFSET_Y);
         for (HashMap.Entry<IconButton, Vocabulary> entry : vocSelectionButtons.entrySet()) {
             IconButton button = entry.getKey();
             button.draw(batch);
