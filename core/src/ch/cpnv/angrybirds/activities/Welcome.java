78e3da08cd7c067269a18e2c207c658708af589e
Damien Jakob <damien.jakob@cpnv.ch>
2020-11-12 09:15:29 +0100

    Update welcome title

--- core/src/ch/cpnv/angrybirds/activities/Welcome.java
+++ core/src/ch/cpnv/angrybirds/activities/Welcome.java
@@ -9,19 +9,32 @@ import ch.cpnv.angrybirds.AngryWirds;
 public class Welcome extends BaseActivity {
     private static final String TITLE = "AngryWirds";
     private static final int TITLE_SCALE = 6;
+    private static final int SUB_TITLE_SCALE = 4;
 
     private BitmapFont titleFont;
     private float titlePositionX;
     private float titlePositionY;
 
+    private BitmapFont subTitleFont;
+    private float subTitlePositionX = 100;
+    private float subTitlePositionY = 150;
+
     public Welcome() {
         titleFont = new BitmapFont();
         titleFont.setColor(Color.ROYAL);
         titleFont.getData().setScale(TITLE_SCALE);
+
+        subTitleFont = new BitmapFont();
+        subTitleFont.setColor(Color.ROYAL);
+        subTitleFont.getData().setScale(SUB_TITLE_SCALE);
+
         GlyphLayout titleGlyphLayout = new GlyphLayout();
         titleGlyphLayout.setText(titleFont, TITLE);
         titlePositionX = Play.WORLD_WIDTH / 2f - titleGlyphLayout.width / 2f;
-        titlePositionY = Play.WORLD_HEIGHT / 2f + titleGlyphLayout.height / 2f;
+        titlePositionY = Play.WORLD_HEIGHT -10;
+
+        subTitlePositionX = 100;
+        subTitlePositionY = titlePositionY - 100;
     }
 
     @Override
@@ -30,6 +43,7 @@ public class Welcome extends BaseActivity {
 
         batch.begin();
         titleFont.draw(batch, TITLE, titlePositionX, titlePositionY);
+        subTitleFont.draw(batch, "blabla", subTitlePositionX, subTitlePositionY);
         batch.end();
     }
 
